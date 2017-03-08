32339dd6758c3dc4d4fa8b1cade224542d1945e4
skyline <rubikandme@outlook.com>
2017-03-08 17:39:22 +0800

    添加page

---
+++ src/page/navigation.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-cat></v-cat>
+</template>
+
+<script>
+  import cat from '../components/nav-catelogue/nav-catelogue.vue'
+  export default {
+    components: {
+      'v-cat': cat
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
