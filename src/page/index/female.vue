30a4bc8d9be1820bb3473de1856118e07bd7c011
skyline <rubikandme@outlook.com>
2017-03-09 17:01:55 +0800

    添加female页面

---
+++ src/page/index/female.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>被发现了,对,这里就是需要大家协助的地方.男生部分已经搞定,有兴趣的话就帮忙建设女生部分吧!</h1>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style scoped>
+  h1 {
+    font-size: 1rem;
+  }
+</style>
