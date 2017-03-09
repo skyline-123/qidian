de34d927fe1e3433893f97822e0bafba2bb89239
skyline <rubikandme@outlook.com>
2017-03-09 17:01:06 +0800

    添加新的路由信息

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import index from '../page/index.vue'
-import nav from '../page/navigation.vue'
+import index from '../page/index/index.vue'
+import femalePage from '../page/index/female.vue'
+import malePage from '../page/index/male.vue'
+import searchPage from '../page/search-page/search-page.vue'
+import category from '../page/category-page.vue'
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
   routes: [
-    { path: '/index', name: 'index', component: index },
-    { path: '/nav', name: 'navigation', component: nav },
-    { path: '*', redirect: '/index' }
+    { path: '/index', name: 'index', component: index, children: [{ path: 'male', component: malePage }, { path: 'female', component: femalePage }] },
+    { path: '/category', name: 'navigation', component: category },
+    { path: '/search', name: 'searchPace', component: searchPage },
+    { path: '*', redirect: '/index/male' }
   ]
 })
