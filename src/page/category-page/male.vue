20cd64fccf1c3bd547976153459514e0e0a32331
skyline <rubikandme@outlook.com>
2017-03-11 21:37:38 +0800

    女生施工模块

---
+++ src/page/category-page/male.vue
@@ -0,0 +1,225 @@
+<template>
+  <div class="category-male">
+    <div class="content">
+      <div class="c-title">
+        <h3 class="title">玄幻</h3>
+        <span class="amount">共558311本作品</span>
+        <span class="more"></span>
+      </div>
+      <div class="c-book-name">
+        <span>东方玄幻</span>
+        <span>异世大陆</span>
+        <span>王朝争霸</span>
+        <span>高武世界</span>
+      </div>
+      <div class="c-title">
+        <h3 class="title">奇幻</h3>
+        <span class="amount">共115883本作品</span>
+        <span class="more"></span>
+      </div>
+      <div class="c-book-name">
+        <span>剑与魔法</span>
+        <span>史诗奇幻</span>
+        <span>黑暗幻想</span>
+        <span>现代魔法</span>
+      </div>
+      <div class="c-title">
+        <h3 class="title">武侠</h3>
+        <span class="amount">共30845本作品</span>
+        <span class="more"></span>
+      </div>
+      <div class="c-book-name">
+        <span>传统武侠</span>
+        <span>武侠幻想</span>
+        <span>国术无双</span>
+      </div>
+      <div class="c-title">
+        <h3 class="title">仙侠</h3>
+        <span class="amount">共195924本作品</span>
+        <span class="more"></span>
+      </div>
+      <div class="c-book-name">
+        <span>修真文明</span>
+        <span>幻想修仙</span>
+        <span>现代修真</span>
+        <span>神话修真</span>
+      </div>
+      <div class="c-title">
+        <h3 class="title">都市</h3>
+        <span class="amount">共303662本作品</span>
+        <span class="more"></span>
+      </div>
+      <div class="c-book-name">
+        <span>都市生活</span>
+        <span>爱情婚姻</span>
+        <span>异术超能</span>
+        <span>恩怨情仇</span>
+        <span>显示百态</span>
+        <span>青春校园</span>
+      </div>
+      <div class="c-title">
+        <h3 class="title">职场</h3>
+        <span class="amount">共20396本作品</span>
+        <span class="more"></span>
+      </div>
+      <div class="c-book-name">
+        <span>娱乐明星</span>
+        <span>官场沉浮</span>
+        <span>商战职场</span>
+      </div>
+      <div class="c-title">
+        <h3 class="title">军事</h3>
+        <span class="amount">共17528本作品</span>
+        <span class="more"></span>
+      </div>
+      <div class="c-book-name">
+        <span>战争幻想</span>
+        <span>谍战特工</span>
+        <span>军旅生涯</span>
+        <span>抗战烽火</span>
+        <span>军事战争</span>
+      </div>
+      <div class="c-title">
+        <h3 class="title">历史</h3>
+        <span class="amount">共63627本作品</span>
+        <span class="more"></span>
+      </div>
+      <div class="c-book-name">
+        <span>外国历史</span>
+        <span>历史专辑</span>
+        <span>架空历史</span>
+        <span>上古先秦</span>
+        <span>秦汉三国</span>
+        <span>两晋隋唐</span>
+        <span>五代十国</span>
+        <span>两宋元明</span>
+        <span>清史明国</span>
+        <span>民间传说</span>
+      </div>
+      <div class="c-title">
+        <h3 class="title">游戏</h3>
+        <span class="amount">共87342本作品</span>
+        <span class="more"></span>
+      </div>
+      <div class="c-book-name">
+        <span>游戏生涯</span>
+        <span>电子竞技</span>
+        <span>虚拟网络</span>
+        <span>游戏异界</span>
+      </div>
+      <div class="c-title">
+        <h3 class="title">体育</h3>
+        <span class="amount">共8692本作品</span>
+        <span class="more"></span>
+      </div>
+      <div class="c-book-name">
+        <span>篮球运动</span>
+        <span>体育赛事</span>
+        <span>足球运动</span>
+      </div>
+      <div class="c-title">
+        <h3 class="title">科幻</h3>
+        <span class="amount">共113586本作品</span>
+        <span class="more"></span>
+      </div>
+      <div class="c-book-name">
+        <span>星际文明</span>
+        <span>时空穿梭</span>
+        <span>未来世界</span>
+        <span>宇宙练功</span>
+        <span>超级科技</span>
+        <span>进化变异</span>
+        <span>末世危机</span>
+      </div>
+      <div class="c-title">
+        <h3 class="title">灵异</h3>
+        <span class="amount">共45561本作品</span>
+        <span class="more"></span>
+      </div>
+      <div class="c-book-name">
+        <span>恐怖惊悚</span>
+        <span>灵异鬼怪</span>
+        <span>悬疑侦探</span>
+        <span>寻墓探险</span>
+        <span>风水秘术</span>
+      </div>
+      <div class="c-title">
+        <h3 class="title">二次元</h3>
+        <span class="amount">共72521本作品</span>
+        <span class="more"></span>
+      </div>
+      <div class="c-book-name">
+        <span>原生幻想</span>
+        <span>衍生同人</span>
+        <span>变身入替</span>
+        <span>搞笑吐槽</span>
+        <span>青春日常</span>
+      </div>
+      <div class="c-title">
+        <h3 class="title">短篇</h3>
+        <span class="more"></span>
+      </div>
+      <div class="c-book-name">
+        <span>诗歌散文</span>
+        <span>人物传记</span>
+        <span>影视剧本</span>
+        <span>生活随笔</span>
+        <span>美文游记</span>
+        <span>儿童文学</span>
+        <span>短篇小说</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style>
+  @import '../../common/css/font/方正中粗雅宋_GBK.css';
+  .content {
+    margin-left: 0.5rem;
+  }
+  .content .c-title {
+    border-bottom: 0.03125rem solid #EFEFEF;
+  }
+  .content .c-title .title {
+    display: inline-block;
+    font-family: "方正中粗雅宋_GBK";
+    font-size: 0.5rem;
+    font-weight: 500;
+    line-height: 1.25rem;
+  }
+  .content .c-title .amount {
+    color: #848B9D;
+    font-size: 0.40625rem;
+    margin-left: 0.28125rem;
+  }
+  .content .c-title .more {
+    display: inline-block;
+    float: right;
+    background: url("arrow-right.png") no-repeat center center;
+    background-size: 0.3125rem 0.40625rem;
+    width: 1.25rem;
+    height: 1.25rem;
+  }
+  .content .c-book-name {
+    padding-bottom: 0.5rem;
+    /*padding-right: 0.5rem;*/
+  }
+  .content .c-book-name span {
+    display: inline-block;
+    line-height: 0.90625rem;
+    width: 2.3125rem;
+    margin-top: 0.375rem;
+    margin-right: 0.625rem;
+    text-align: center;
+    color: #848B9D;
+    border: 0.03125rem solid #848B9D;
+    border-radius: 1.5625rem;
+  }
+  .content .c-book-name span:last-child {
+    margin-right: 0;
+  }
+</style>
